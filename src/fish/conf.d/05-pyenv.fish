1a0ad2234075b0a381b35220aadeac088dadb14b
Attila GOLONCSER <agl@kxxp.net>
2025-03-19 15:15:24 +0100

    Fix pyenv initialization in fish

--- src/fish/conf.d/05-pyenv.fish
+++ src/fish/conf.d/05-pyenv.fish
@@ -1,3 +1,4 @@
 # https://github.com/pyenv/pyenv?tab=readme-ov-file#b-set-up-your-shell-environment-for-pyenv
 set -Ux PYENV_ROOT $HOME/.pyenv
 set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+pyenv init - fish | source
