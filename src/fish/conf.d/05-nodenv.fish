642bdca45c6ab6842cf94e17cbb57dcff05c4aea
Attila GOLONCSER <agl@kxxp.net>
2025-03-20 12:31:10 +0100

    Fix tool initialization [skip ci]

--- src/fish/conf.d/05-nodenv.fish
+++ src/fish/conf.d/05-nodenv.fish
@@ -1,3 +1,5 @@
 # https://github.com/nodenv/nodenv#basic-github-checkout
 set -Ux fish_user_paths $HOME/.nodenv/bin $fish_user_paths
-status --is-interactive; and nodenv init - fish | source
+if test -x $HOME/.nodenv/bin/nodenv
+    status --is-interactive; and nodenv init - fish | source
+end
--- src/fish/conf.d/05-pyenv.fish
+++ src/fish/conf.d/05-pyenv.fish
@@ -1,4 +1,6 @@
 # https://github.com/pyenv/pyenv?tab=readme-ov-file#b-set-up-your-shell-environment-for-pyenv
 set -Ux PYENV_ROOT $HOME/.pyenv
 set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
-pyenv init - fish | source
+if test -x $PYENV_ROOT/bin/pyenv
+    pyenv init - fish | source
+end
