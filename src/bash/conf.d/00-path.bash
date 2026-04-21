a78c8e1d4ac951ab58c42ffdd78de33a88417812
Attila Gölöncsér <zgolatt@seroiuvd07352.sero.gic.ericsson.se>
2026-04-21 16:24:00 +0200

    fix: Add snap to shell PATH

--- src/bash/conf.d/00-path.bash
+++ src/bash/conf.d/00-path.bash
@@ -1,1 +1,1 @@
-export PATH=${HOME}/.local/bin:${PATH}
+export PATH=/snap/bin:${HOME}/.local/bin:${PATH}
--- src/fish/conf.d/00-path.fish
+++ src/fish/conf.d/00-path.fish
@@ -1,5 +1,6 @@
 if functions -q fish_add_path
     fish_add_path $HOME/.local/bin
+    fish_add_path /snap/bin
 else
-    set -Ux fish_user_paths $HOME/.local/bin $fish_user_paths
+    set -Ux fish_user_paths /snap/bin $HOME/.local/bin $fish_user_paths
 end
