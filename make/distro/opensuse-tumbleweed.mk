13ada0dc334e66007c8c7361aa2ea4f01ffb7a8a
Attila GOLONCSER <agl@glnc.hu>
2026-06-23 13:40:10 +0200

    Add golang to default install

--- make/distro/opensuse-tumbleweed.mk
+++ make/distro/opensuse-tumbleweed.mk
@@ -1,6 +1,6 @@
 PACKAGES += bash curl fish git gpg2 mosh password-store pass-otp
 PACKAGES += pcsc-lite pcsc-ccid tmux rsync fzf git-lfs git-annex
-PACKAGES += openssh-server gawk tar man-pages sudo
+PACKAGES += openssh-server gawk tar man-pages sudo golang
 
 .PHONY : opensuse-tumbleweed-upgrade
 opensuse-tumbleweed-upgrade :
